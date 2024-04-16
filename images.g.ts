7aa393c6a8acfe4be5e79e1fd053e6d691adbaea
Lesbian-God <167255455+Lesbian-God@users.noreply.github.com>
2024-04-16 15:56:50 -0600

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,222 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "akita":return img`
+bbbb........bbbb.................
+c99bb......bb99b.................
+c999bb....bb999c.................
+c9b99bccccb99b9c.................
+c9bb99bccb99bb9c.................
+c93b99999999b39c.................
+c93399999999339c.................
+c99399999999399c.................
+c99999991199999c.................
+c999ff91119ff99c........bbbbbb...
+c999ff91111ff99c.......c999999bb.
+c99991111111999c......c99999999b.
+c9991111fff1199c.....c9991119999b
+c999c11fff1199bc.....c9911111999b
+c999cc111111c9bc.....c911dd11199b
+c99999bb33cc99bcc....cbddbbd1199c
+c999999b33c99999bbccccbbdbbb1199c
+c9999999bb9999999999999999bb1999c
+c999911119999999999999999999b999c
+c999111111999999999999999999999c.
+c99911111119999999999999999999cc.
+c99111111119999999999999999999c..
+c99111111111999999999999999999c..
+cb9111111111999999999999999999c..
+.f9111111111999999999999999999c..
+.ff111111111999999999999999999c..
+..fb11111111999999999999999999c..
+...fb1111119999999111111999999c..
+...fbbb11119999991111111199999c..
+....fbbfffb9999ccccccccccb9999c..
+....fbbf..f999c.....fbbf.c9999c..
+....fbbf..f999c.....fbbf.cc9999c.
+....fbbf..f99c.......fbf..cc999c.
+....fbbf..f99c.......fbbf..cc99c.
+....fbbf..f99c.......fbbf...c99c.
+....fbbf..f99c......fbbbf...c99c.
+...fbbbf..f99c......ffff....cb9c.
+...fbbf..f999c.............c999c.
+...ffff..f99cc.............c999c.
+.........fffc..............cccc..
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "myAnim":
+            case "anim1":return [img`
+f f f f f f f f f f f f f f f f 
+f e e e e e e e e e e e e e e f 
+f e d d d d d d d d d d d d e f 
+f e d c c c c c c c c c c d e f 
+f e d c b b b b b b b b c d e f 
+f e d c b a a a a a a b c d e f 
+f e d c b a 9 9 9 9 a b c d e f 
+f e d c b a 9 8 8 9 a b c d e f 
+f e d c b a 9 8 8 9 a b c d e f 
+f e d c b a 9 9 9 9 a b c d e f 
+f e d c b a a a a a a b c d e f 
+f e d c b b b b b b b b c d e f 
+f e d c c c c c c c c c c d e f 
+f e d d d d d d d d d d d d e f 
+f e e e e e e e e e e e e e e f 
+f f f f f f f f f f f f f f f f 
+`, img`
+e e e e e e e e e e e e e e e e 
+e d d d d d d d d d d d d d d e 
+e d c c c c c c c c c c c c d e 
+e d c b b b b b b b b b b c d e 
+e d c b a a a a a a a a b c d e 
+e d c b a 9 9 9 9 9 9 a b c d e 
+e d c b a 9 8 8 8 8 9 a b c d e 
+e d c b a 9 8 f f 8 9 a b c d e 
+e d c b a 9 8 f f 8 9 a b c d e 
+e d c b a 9 8 8 8 8 9 a b c d e 
+e d c b a 9 9 9 9 9 9 a b c d e 
+e d c b a a a a a a a a b c d e 
+e d c b b b b b b b b b b c d e 
+e d c c c c c c c c c c c c d e 
+e d d d d d d d d d d d d d d e 
+e e e e e e e e e e e e e e e e 
+`, img`
+d d d d d d d d d d d d d d d d 
+d c c c c c c c c c c c c c c d 
+d c b b b b b b b b b b b b c d 
+d c b a a a a a a a a a a b c d 
+d c b a 9 9 9 9 9 9 9 9 a b c d 
+d c b a 9 8 8 8 8 8 8 9 a b c d 
+d c b a 9 8 f f f f 8 9 a b c d 
+d c b a 9 8 f e e f 8 9 a b c d 
+d c b a 9 8 f e e f 8 9 a b c d 
+d c b a 9 8 f f f f 8 9 a b c d 
+d c b a 9 8 8 8 8 8 8 9 a b c d 
+d c b a 9 9 9 9 9 9 9 9 a b c d 
+d c b a a a a a a a a a a b c d 
+d c b b b b b b b b b b b b c d 
+d c c c c c c c c c c c c c c d 
+d d d d d d d d d d d d d d d d 
+`, img`
+c c c c c c c c c c c c c c c c 
+c b b b b b b b b b b b b b b c 
+c b a a a a a a a a a a a a b c 
+c b a 9 9 9 9 9 9 9 9 9 9 a b c 
+c b a 9 8 8 8 8 8 8 8 8 9 a b c 
+c b a 9 8 f f f f f f 8 9 a b c 
+c b a 9 8 f e e e e f 8 9 a b c 
+c b a 9 8 f e d d e f 8 9 a b c 
+c b a 9 8 f e d d e f 8 9 a b c 
+c b a 9 8 f e e e e f 8 9 a b c 
+c b a 9 8 f f f f f f 8 9 a b c 
+c b a 9 8 8 8 8 8 8 8 8 9 a b c 
+c b a 9 9 9 9 9 9 9 9 9 9 a b c 
+c b a a a a a a a a a a a a b c 
+c b b b b b b b b b b b b b b c 
+c c c c c c c c c c c c c c c c 
+`, img`
+b b b b b b b b b b b b b b b b 
+b a a a a a a a a a a a a a a b 
+b a 9 9 9 9 9 9 9 9 9 9 9 9 a b 
+b a 9 8 8 8 8 8 8 8 8 8 8 9 a b 
+b a 9 8 f f f f f f f f 8 9 a b 
+b a 9 8 f e e e e e e f 8 9 a b 
+b a 9 8 f e d d d d e f 8 9 a b 
+b a 9 8 f e d c c d e f 8 9 a b 
+b a 9 8 f e d c c d e f 8 9 a b 
+b a 9 8 f e d d d d e f 8 9 a b 
+b a 9 8 f e e e e e e f 8 9 a b 
+b a 9 8 f f f f f f f f 8 9 a b 
+b a 9 8 8 8 8 8 8 8 8 8 8 9 a b 
+b a 9 9 9 9 9 9 9 9 9 9 9 9 a b 
+b a a a a a a a a a a a a a a b 
+b b b b b b b b b b b b b b b b 
+`, img`
+a a a a a a a a a a a a a a a a 
+a 9 9 9 9 9 9 9 9 9 9 9 9 9 9 a 
+a 9 8 8 8 8 8 8 8 8 8 8 8 8 9 a 
+a 9 8 f f f f f f f f f f 8 9 a 
+a 9 8 f e e e e e e e e f 8 9 a 
+a 9 8 f e d d d d d d e f 8 9 a 
+a 9 8 f e d c c c c d e f 8 9 a 
+a 9 8 f e d c b b c d e f 8 9 a 
+a 9 8 f e d c b b c d e f 8 9 a 
+a 9 8 f e d c c c c d e f 8 9 a 
+a 9 8 f e d d d d d d e f 8 9 a 
+a 9 8 f e e e e e e e e f 8 9 a 
+a 9 8 f f f f f f f f f f 8 9 a 
+a 9 8 8 8 8 8 8 8 8 8 8 8 8 9 a 
+a 9 9 9 9 9 9 9 9 9 9 9 9 9 9 a 
+a a a a a a a a a a a a a a a a 
+`, img`
+9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 
+9 8 8 8 8 8 8 8 8 8 8 8 8 8 8 9 
+9 8 f f f f f f f f f f f f 8 9 
+9 8 f e e e e e e e e e e f 8 9 
+9 8 f e d d d d d d d d e f 8 9 
+9 8 f e d c c c c c c d e f 8 9 
+9 8 f e d c b b b b c d e f 8 9 
+9 8 f e d c b a a b c d e f 8 9 
+9 8 f e d c b a a b c d e f 8 9 
+9 8 f e d c b b b b c d e f 8 9 
+9 8 f e d c c c c c c d e f 8 9 
+9 8 f e d d d d d d d d e f 8 9 
+9 8 f e e e e e e e e e e f 8 9 
+9 8 f f f f f f f f f f f f 8 9 
+9 8 8 8 8 8 8 8 8 8 8 8 8 8 8 9 
+9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 
+`, img`
+8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
+8 f f f f f f f f f f f f f f 8 
+8 f e e e e e e e e e e e e f 8 
+8 f e d d d d d d d d d d e f 8 
+8 f e d c c c c c c c c d e f 8 
+8 f e d c b b b b b b c d e f 8 
+8 f e d c b a a a a b c d e f 8 
+8 f e d c b a 9 9 a b c d e f 8 
+8 f e d c b a 9 9 a b c d e f 8 
+8 f e d c b a a a a b c d e f 8 
+8 f e d c b b b b b b c d e f 8 
+8 f e d c c c c c c c c d e f 8 
+8 f e d d d d d d d d d d e f 8 
+8 f e e e e e e e e e e e e f 8 
+8 f f f f f f f f f f f f f f 8 
+8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
+`, img`
+f f f f f f f f f f f f f f f f 
+f e e e e e e e e e e e e e e f 
+f e d d d d d d d d d d d d e f 
+f e d c c c c c c c c c c d e f 
+f e d c b b b b b b b b c d e f 
+f e d c b a a a a a a b c d e f 
+f e d c b a 9 9 9 9 a b c d e f 
+f e d c b a 9 8 8 9 a b c d e f 
+f e d c b a 9 8 8 9 a b c d e f 
+f e d c b a 9 9 9 9 a b c d e f 
+f e d c b a a a a a a b c d e f 
+f e d c b b b b b b b b c d e f 
+f e d c c c c c c c c c c d e f 
+f e d d d d d d d d d d d d e f 
+f e e e e e e e e e e e e e e f 
+f f f f f f f f f f f f f f f f 
+`];
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
